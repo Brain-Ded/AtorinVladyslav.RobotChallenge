623ddb9dab841afef0171edca4c6fd115f16c257
shado <shado@DESKTOP-7POQ0P0>
2022-08-21 13:17:17 +0300

    Add project files.

---
+++ AtorinVladyslav.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace AtorinVladyslav.RobotChallenge
+{
+    public class AtorinVladyslav : IRobotAlgorithm
+    {
+        public string Author => throw new NotImplementedException();
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
